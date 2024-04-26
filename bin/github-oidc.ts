d11bcd250eb0f29b03893bd64135a23c43ffc894
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2024-04-26 18:48:54 +0200

    update github OIDC stack

--- bin/github-oidc.ts
+++ bin/github-oidc.ts
@@ -2,11 +2,6 @@
 import 'source-map-support/register'
 import * as cdk from '@aws-cdk/core'
 import { GithubOidcStack } from '../lib/github-oidc-stack'
-import { TestInfraStack } from '../lib/test-infra-stack'
 
 const app = new cdk.App()
 new GithubOidcStack(app, 'GithubOidcStack', { env: { region: 'us-east-2' } })
-
-new TestInfraStack(app, 'TestInfraStack', {
-  env: { account: process.env.AWS_ACCOUNT_ID, region: 'us-east-2' },
-})
